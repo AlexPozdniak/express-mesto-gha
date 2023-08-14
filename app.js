94239c381176e6efcae5d9896d77ca29d70dac7a
AlexPozdniak <sotich911@gmail.com>
2023-08-15 01:16:14 +0300

    13

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
-  _id: '64da8beac540897f04c68625' 
-};
-next();
+    _id: '64da8beac540897f04c68625'
+  };
+  next();
 });
 
 app.use(router);
