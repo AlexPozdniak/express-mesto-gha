0047aa2333e7118b8be71708d1652d7ce2ce193b
AlexPozdniak <sotich911@gmail.com>
2023-09-05 21:53:25 +0300

    1

--- errors/conflict.js
+++
@@ -1,9 +0,0 @@
-const { conflict } = require('./errorCodes');
-
-class Conflict extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = conflict;
-  }
-}
-module.exports = Conflict;
